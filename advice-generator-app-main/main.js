3ba7362daad78a1aec86a70b4704101be096d20b
Rasmi pun <rasmipurjapun@gmail.com>
2022-12-25 11:01:33 +0545

    first commit

---
+++ advice-generator-app-main/main.js
@@ -0,0 +1,13 @@
+const adviceId = document.getElementById("advice-id");
+const adviceText = document.getElementById("advice-text");
+const adviceButton = document.getElementById("generate-btn");
+
+async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const {slip:{id,advice}} = await res.json();
+    adviceId.innerText = id;
+    adviceText.innerText = advice;
+}
+
+getAdvice()
+adviceButton.addEventListener("click",getAdvice)
